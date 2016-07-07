40e0ef28f81fad79778b087e08a3cc9aaef67417
CharadeMao <charade.mao@gmail.com>
2016-07-07 19:22:34 +0800

    deploy scripy beta

---
+++ upload/deploy.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * GIT DEPLOYMENT SCRIPT
+ */
+// The commands
+$commands = array(
+    "D:\Git\bin\git pull",
+);
+// Run the commands for output
+$output = '';
+
+foreach($commands AS $command){
+    $tmp = exec($command);
+    $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+    $output .= htmlentities(trim($tmp)) . "\n";
+}
+
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>GIT DEPLOYMENT SCRIPT</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<pre>
+ .  ____  .    ____________________________
+ |/      \|   |                            |
+[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
+ |___==___|  /              &copy; oodavid 2012 |
+              |____________________________|
+
+    <?php echo $output; ?>
+</pre>
+</body>
+</html>
